2f33e1b04e47a4a13586c6cca75df9d1342cee15
Chiu-Ki Chan <chiuki@gmail.com>
2015-01-20 17:18:16 -0700

    Only show links in AboutActivity if the device can open them.
    Fixes #3.

--- app/src/main/java/com/sqisland/android/advanced_textview/AboutActivity.java
+++ app/src/main/java/com/sqisland/android/advanced_textview/AboutActivity.java
@@ -1,10 +1,17 @@
 package com.sqisland.android.advanced_textview;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class AboutActivity extends Activity {
   @Override
@@ -13,8 +20,21 @@ public class AboutActivity extends Activity {
     setContentView(R.layout.activity_about);
 
     TextView textView = (TextView) findViewById(R.id.text);
-    String html = getString(R.string.about_text);
-    textView.setText(Html.fromHtml(html));
-    textView.setMovementMethod(LinkMovementMethod.getInstance());
+    CharSequence html = Html.fromHtml(getString(R.string.about_text));
+
+    if (canOpenLinks()) {
+      textView.setText(html);
+      textView.setMovementMethod(LinkMovementMethod.getInstance());
+    } else {
+      textView.setText(html.toString());
+    }
+  }
+
+  public boolean canOpenLinks() {
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://d.android.com"));
+    final PackageManager packageManager = getPackageManager();
+    List<ResolveInfo> list = packageManager.queryIntentActivities(
+        intent, PackageManager.MATCH_DEFAULT_ONLY);
+    return list.size() > 0;
   }
 }
